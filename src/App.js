8aa47274799fd2020acb9776dd29a24f18c962cc
Nada <nada.tebba@hotmail.fr>
2020-06-17 19:30:40 +0200

    correction

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, HashRouter,Route,Switch } from 'react-router-dom';
+import { HashRouter as Router,Route,Switch } from 'react-router-dom';
 import {Navigation,Footer,Home, About, Contact} from './components';
 import './style.css';
 
