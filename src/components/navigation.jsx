de09b71d52bfb73f9cd780e20c267c50e4d86235
Nada <nada.tebba@hotmail.fr>
2020-06-17 18:42:32 +0200

    change url

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -5,11 +5,11 @@ import { Link, withRouter } from "react-router-dom";
 const Navigation=()=>{
     return(
         <nav>
-            <h1><Link className='navigation' to='/'>React multi pages website</Link></h1>
+            <h1><Link className='navigation' to='/mutlti-pages-react-website/'>React multi pages website</Link></h1>
             <ul>
-                <Link className='navigation' to='/'><li>Home</li></Link>
-                <Link className='navigation' to='/about'><li>About</li></Link>
-                <Link className='navigation' to='/contact'><li>Contact</li></Link>
+                <Link className='navigation' to='/mutlti-pages-react-website/'><li>Home</li></Link>
+                <Link className='navigation' to='mutlti-pages-react-website/about'><li>About</li></Link>
+                <Link className='navigation' to='mutlti-pages-react-website/contact'><li>Contact</li></Link>
             </ul>    
         </nav>
     );
